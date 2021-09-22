c4d7abb7547e7b83ecad6bedfea1865bcce39a0d
yuto-yuto <yuto.nakatsu@gmail.com>
2021-09-22 20:49:29 +0200

    Add test

--- test/item_tree_generator_test.dart
+++ test/item_tree_generator_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:grouped_expansion_tile/grouped_expansion_tile.dart';
 import 'package:grouped_expansion_tile/item_tree_generator.dart';
@@ -25,19 +26,50 @@ void main() {
       expect(result, isEmpty);
     });
     test("should return only top parents when they don't have child", () {
-      // final data = [
-      //   GroupBase(uid: "1"),
-      //   GroupBase(uid: "2"),
-      // ];
-      // final result = createItemTree(data);
-      // expect(result.length, 2);
-      // expect(result, [
-      //   Parent(self: GroupBase(uid: "1")),
-      //   Parent(self: GroupBase(uid: "2")),
-      // ]);
-      // expect(result[0].children, null);
-      // expect(result[0].self.uid, "1");
-      // expect(result[0].self.parent, );
+      final data = [
+        GroupBase(uid: "1"),
+        GroupBase(uid: "2"),
+      ];
+      final result = createItemTree(data);
+      expect(result.length, 2);
+      final expected = [
+        Parent(self: GroupBase(uid: "1")),
+        Parent(self: GroupBase(uid: "2")),
+      ];
+      expect(listEquals(result, expected), true);
+    });
+    test("should return complete Parent tree", () {
+      final data = [
+        GroupBase(uid: "1"),
+        GroupBase(uid: "2"),
+        GroupBase(uid: "3", parent: "1"),
+        GroupBase(uid: "4", parent: "1"),
+        GroupBase(uid: "5", parent: "2"),
+        GroupBase(uid: "6", parent: "2"),
+        GroupBase(uid: "7", parent: "4"),
+      ];
+      final result = createItemTree(data);
+      expect(result.length, 2);
+      final expected = [
+        Parent(
+          self: GroupBase(uid: "1"),
+          children: [
+            Parent(self: GroupBase(uid: "3", parent: "1")),
+            Parent(
+              self: GroupBase(uid: "4", parent: "1"),
+              children: [Parent(self: GroupBase(uid: "7", parent: "4"))],
+            ),
+          ],
+        ),
+        Parent(
+          self: GroupBase(uid: "2"),
+          children: [
+            Parent(self: GroupBase(uid: "5", parent: "2")),
+            Parent(self: GroupBase(uid: "6", parent: "2")),
+          ],
+        ),
+      ];
+      expect(listEquals(result, expected), true);
     });
   });
 }
